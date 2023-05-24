9ee16225867be4b086cb04fe043b52d59afb5137
Baxter <davidf.martinez.br@gmail.com>
2023-05-24 12:12:14 -0500

    Added product details functionality

--- main.js
+++ main.js
@@ -102,43 +102,42 @@ productList.push({
 */
 
 const renderProducts = (arr) => {
-    for (product of arr) {
-        const productCard = document.createElement('div');
-        productCard.classList.add('product-card');
-    
-        // product = {name, price, image} -> product.image
-        const productImg = document.createElement('img');
-        productImg.setAttribute('src', product.image);
-    
-        const productInfo = document.createElement('div');
-        productInfo.classList.add('product-info');
-    
-        const productInfoDiv = document.createElement('div');
-    
-        const productPrice = document.createElement('p');
-        productPrice.innerText = '$' + product.price;
-    
-        const productName = document.createElement('p');
-        productName.innerText = product.name;
-    
-        productInfoDiv.appendChild(productPrice);
-        productInfoDiv.appendChild(productName);
-    
-        const productInfoFigure = document.createElement('figure');
-        const productImgCard = document.createElement('img');
-        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
-    
-        productInfoFigure.appendChild(productImgCard);
-    
-        productInfo.appendChild(productInfoDiv);
-        productInfo.appendChild(productInfoFigure);
-    
-        productCard.appendChild(productImg);
-        productCard.appendChild(productInfo);
-    
-        cardsContainer.appendChild(productCard);
-    }
-}
+	for (product of arr) {
+		const productCard = document.createElement('div');
+		productCard.classList.add('product-card');
+
+		// product = {name, price, image} -> product.image
+		const productImg = document.createElement('img');
+		productImg.setAttribute('src', product.image);
+
+		const productInfo = document.createElement('div');
+		productInfo.classList.add('product-info');
+
+		const productInfoDiv = document.createElement('div');
+
+		const productPrice = document.createElement('p');
+		productPrice.innerText = '$' + product.price;
+
+		const productName = document.createElement('p');
+		productName.innerText = product.name;
+
+		productInfoDiv.appendChild(productPrice);
+		productInfoDiv.appendChild(productName);
+
+		const productInfoFigure = document.createElement('figure');
+		const productImgCard = document.createElement('img');
+		productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+		productInfoFigure.appendChild(productImgCard);
+
+		productInfo.appendChild(productInfoDiv);
+		productInfo.appendChild(productInfoFigure);
+
+		productCard.appendChild(productImg);
+		productCard.appendChild(productInfo);
+
+		cardsContainer.appendChild(productCard);
+	}
+};
 
 renderProducts(productList);
-
