d31228edb10c994685003c75e09b4b37de4c2981
Baxter <davidf.martinez.br@gmail.com>
2023-05-24 11:21:45 -0500

    Added cart functionality

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
+/** Desktop Functionality */
 const navEmail = document.querySelector('.navbar-email');
 
-
 const toggleDesktopMenu = () => {
-    // console.log('click');
+	const isAsideCartClosed = asideCart.classList.contains('inactive');
+
+	if (!isAsideCartClosed) {
+		asideCart.classList.add('inactive');
+	}
+
 	desktopMenu.classList.toggle('inactive');
 };
 
 const desktopMenu = document.querySelector('.desktop-menu');
 navEmail.addEventListener('click', toggleDesktopMenu);
 
-// console.log('Funciona');
+/** Mobile Functionality */
+const menuHamIcon = document.querySelector('.menu');
+const toggleMobileMenu = () => {
+	const isAsideCartClosed = asideCart.classList.contains('inactive');
+
+	if (!isAsideCartClosed) {
+		asideCart.classList.add('inactive');
+	}
+
+	mobileMenu.classList.toggle('inactive');
+};
+
+const mobileMenu = document.querySelector('.mobile-menu');
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+/** Cart Functionality */
+const menuCarIcon = document.querySelector('.navbar-shopping-cart');
+const toggleCartMenu = () => {
+	const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+	if (!isMobileMenuClosed) {
+		mobileMenu.classList.add('inactive');
+	}
+
+	asideCart.classList.toggle('inactive');
+};
+
+const asideCart = document.querySelector('.product-detail');
+menuCarIcon.addEventListener('click', toggleCartMenu);
